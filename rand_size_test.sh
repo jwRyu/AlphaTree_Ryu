2669e6b2e8a99ad4df6a0c6e0bf57faf12f6eeb3
jiwoo <jwryu1128@gmail.com>
2024-10-27 17:42:35 +0100

    More slurm stuff

---
+++ rand_size_test.sh
@@ -0,0 +1,28 @@
+./AlphaTree config/config_hybrid_64bit_4N_1000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_4N_2000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_4N_4000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_4N_8000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_4N_12000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_4N_16000.txt | tee -a output/out_size_test.txt
+
+./AlphaTree config/config_hybrid_64bit_8N_1000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_8N_2000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_8N_4000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_8N_8000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_8N_12000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hybrid_64bit_8N_16000.txt | tee -a output/out_size_test.txt
+
+./AlphaTree config/config_hq_8bit_4N_1000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_4N_2000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_4N_4000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_4N_8000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_4N_12000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_4N_16000.txt | tee -a output/out_size_test.txt
+
+./AlphaTree config/config_hq_8bit_8N_1000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_8N_2000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_8N_4000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_8N_8000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_8N_12000.txt | tee -a output/out_size_test.txt
+./AlphaTree config/config_hq_8bit_8N_16000.txt | tee -a output/out_size_test.txt
+
--- test_script.sh
+++ test_script.sh
@@ -1,3 +1,3 @@
 #./AlphaTree test_config.txt | tee -a test_out.txt
 
-./AlphaTree config/config_hybrid_64bit_8N.txt | tee -a output/out_hybrid.txt
+./AlphaTree config/config_hybrid_64bit_8N_thr1.txt | tee -a output/out_hybrid_thr1_64bit.txt
